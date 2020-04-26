844c41b62beec7d84129db920257aa0edf2e4784
Archit Bhonsle <abhonsle2000@gmail.com>
2020-04-26 16:23:21 +0530

    Shifting shop data to firebase and implementing WithSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,71 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
+
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
-export default ShopPage;
+class ShopPage extends React.Component {
+  state = {
+    loading : true
+  };
+
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    // // Observable pattern
+    // collectionRef.onSnapshot(async (snapShot) => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // });
+
+    // Promise pattern
+    collectionRef.get().then((snapShot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+
+    // // Fetch/API pattern
+    // fetch(
+    //   'https://firestore.googleapis.com/v1/projects/e-commerce-react-8120b/databases/(default)/documents/collections'
+    // )
+    //   .then((response) => response.json)
+    //   .then((collections) => console.log(collections));
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className='shop-page'>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections : (collectionsMap) => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -4,9 +4,13 @@ const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector([ selectShop ], (shop) => shop.collections);
 
-export const selectCollectionsForPreview = createSelector([ selectCollections ], (collections) =>
-  Object.keys(collections).map((key) => collections[key])
+export const selectCollectionsForPreview = createSelector(
+  [ selectCollections ],
+  (collections) => (collections ? Object.keys(collections).map((key) => collections[key]) : [])
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([ selectCollections ], (collections) => collections[collectionUrlParam]);
+  createSelector(
+    [ selectCollections ],
+    (collections) => (collections ? collections[collectionUrlParam] : null)
+  );
