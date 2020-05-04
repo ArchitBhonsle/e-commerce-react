28afa64692b37d50cc1a0fedcdb71936543a5f5a
Archit Bhonsle <abhonsle2000@gmail.com>
2020-05-04 16:23:26 +0530

    Making our app PWA compliant

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
 
+import * as serviceWorker from './serviceWorker';
+
 import './index.css';
 import App from './App';
 
@@ -19,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -14,6 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
@@ -37,6 +39,10 @@ app.post('/payment', (req, res) => {
       res.status(200).send({ success: stripeRes });
     }
   });
+});
+
+app.get('./serviceWorker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'serviceWorker.js'));
 });
 
 app.listen(port, (error) => {
